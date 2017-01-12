08565ebe742f74cf82102429ff8bfe12a3cc3bb6
Aaron Schut <schutaps@gmail.com>
2017-01-12 13:35:55 -0800

    something new

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'pins#index'
-  get 'about' => 'pages#about'
+  get 'now' => 'pages#now'
+  get 'citeandquote' => 'pages#citeandquote'
+  get 'compass' => 'pages#compass'
+  get 'toolbox' => 'pages#toolbox'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
